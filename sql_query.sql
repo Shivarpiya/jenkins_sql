8039bca4596b6eee101bc626ff4b02c34ff96522
Shivarpiya <44540060+Shivarpiya@users.noreply.github.com>
2019-08-02 08:38:22 +0000

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -1,2 +1,8 @@
 SELECT 'some text'
-CREATE DATABASE NEW2;
+use Jenkins_employees;
+CREATE TABLE [employees1](
+	[emp_id] [int] NOT NULL,
+	[job_title] [varchar](255) NULL,
+	[joining_date] [date] NULL,
+	[description] [text] NULL,
+);
