0488a68fdb106f3464769304d6cf994e04f42d7d
Shivarpiya <44540060+Shivarpiya@users.noreply.github.com>
2019-08-02 06:53:05 +0000

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -1,8 +1,2 @@
 SELECT 'some text'
-CREATE TABLE dbo.employees3(
-    emp_id INT,
-    job_title VARCHAR(255),
-    joining_date DATE,
-    description TEXT,
-    PRIMARY KEY (emp_id)
-);
+CREATE DATABASE NEW1;
