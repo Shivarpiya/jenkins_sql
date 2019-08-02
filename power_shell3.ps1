f3d283acfb45edea23899f65aa3575e96641fb07
Shivarpiya <44540060+Shivarpiya@users.noreply.github.com>
2019-08-02 07:01:18 +0000

    Update power_shell3.ps1

--- power_shell3.ps1
+++ power_shell3.ps1
@@ -8,9 +8,10 @@ $sqlUserID='SenthilEPVM2017\SenthilEPVS2017'
 $sqlPassword='M0ns00nR@!ns#20!9'
 $windowsUserName="SenthilEPVM2017\SenthilEPVS2017"
 
+
 "connected"
 $SqlScriptQueries  = get-content -path "C:\Users\SenthilEPVS2017\.jenkins\workspace\sql_project\sql_query.sql"
-$connectionDetails = "Provider=sqloledb; " + "Data Source=$dataSource; " + "Initial Catalog=$database; " + "Integrated Security=SSPI;"+"User ID=$windowsUserName;Password=$windowsPassword"
+$connectionDetails = "Provider=sqloledb; " + "Data Source=$dataSource; " + "Initial Catalog=$database; " + "Integrated Security=SSPI;"
 $connection = New-Object System.Data.OleDb.OleDbConnection $connectionDetails
 "connection successful"
 $connection.open()
