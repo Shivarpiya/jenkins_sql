1e516216258c38db6ef26d81acf4a6f65b9e5bb0
Shivarpiya <44540060+Shivarpiya@users.noreply.github.com>
2019-08-02 11:35:09 +0000

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -1,3 +1,3 @@
 SELECT 'some text'
 use Jenkins_employees;
-INSERT INTO employees3 (emp_id,job_title,description) VALUES ('25','SA','aws');
+INSERT INTO employees3 (emp_id,job_title,description) VALUES ('100','SA','aws');
